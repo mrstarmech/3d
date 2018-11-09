6708fdde455f61d7aba2ca08cfbb4da7479a4470
Каир Жумадилов <k.zhumadilov@g.nsu.ru>
2018-11-09 14:06:53 +0700

    shot

--- views/admin/shot.php
+++ views/admin/shot.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use \yii\widgets\ActiveForm;
 
 $this->registerJsFile('js/lib.tree.js', ['depends' => ['yii\bootstrap\BootstrapPluginAsset']]);
 $this->registerJsFile('js/viewer.js', ['depends' => ['yii\bootstrap\BootstrapPluginAsset']]);
@@ -41,9 +42,59 @@ object = {
     labels: $labelsJson,
 };
 host = '$host';
+object.option.preserveDrawingBuffer = true;
+object.option.menuDisable = true;
 
 start();
 
+var strDownloadMime = "image/octet-stream";
+$('#saveLink').click(function() {
+  saveAsImage();
+})
+    function saveAsImage() {
+        var imgData, imgNode;
+
+        try {
+            var strMime = "image/jpeg";
+            imgData = t.renderer.domElement.toDataURL(strMime);
+            // saveFile(imgData.replace(strMime, strDownloadMime), "$object->name.jpg");
+            $('input[name=data]').val(imgData);
+            $('input[name=coordinateX]').val(t.camera.position.x);
+            $('input[name=coordinateY]').val(t.camera.position.y);
+            $('input[name=coordinateZ]').val(t.camera.position.z);
+            
+//            var csrfToken = $('meta[name="csrf-token"]').attr("content");
+//            $.ajax({
+//               async: false,
+//               type: "POST",
+//               dataType: "json",
+//               url: "/admin/shot/$object->id",
+//               data: {data: imgData, _csrf: csrfToken},
+//               cache: false,
+//               success: function(response){
+//                   console.log(response);
+//               }
+//           });
+
+        } catch (e) {
+            console.log(e);
+            return;
+        }
+        return false;
+
+    }
+    var saveFile = function (strData, filename) {
+        var link = document.createElement('a');
+        if (typeof link.download === 'string') {
+            document.body.appendChild(link); //Firefox requires the link to be in the body
+            link.download = filename;
+            link.href = strData;
+            link.click();
+            document.body.removeChild(link); //remove the link when done
+        } else {
+            location.replace(uri);
+        }
+    }
 JS;
 
 $this->registerJs($script, yii\web\View::POS_READY);
@@ -55,16 +106,25 @@ echo $this->render('_header');
 echo $this->render('_header_object', ['id' => $object->id]);
 ?>
 <div class="col-xs-12 col-md-6 tree-object">
-    <div class="container-object" data-state="static"
-         style="background: url(<?= '/' . $object->pathImage . '/' . $object->id . '/' . $object->image ?>) center center / cover;">
+    <div class="container-object" data-state="static">
         <div class="canvas-object"></div>
     </div>
 </div>
-<?php if (!Yii::$app->user->isGuest): // TODO: admin check ?>
-    <div class="pull-right">
-        <?= Html::a('Edit', ['admin/edit-object-general', 'id' => $object->id], ['class' => 'btn btn-primary']) ?>
-    </div>
-<?php endif; ?>
 <h1><?= $this->title ?></h1>
 
-<?= $object->description ?>
+<div class="form-group">
+    <button id="saveLink" class="btn btn-primary">
+        Сохранить
+    </button>
+</div>
+
+<?php $form = ActiveForm::begin(); ?>
+<input type="hidden" name="data">
+<input type="hidden" name="coordinateX">
+<input type="hidden" name="coordinateY">
+<input type="hidden" name="coordinateZ">
+<div class="form-group">
+    <?= Html::submitButton('Добавить', ['class' => 'btn btn-primary']) ?>
+</div>
+
+<?php ActiveForm::end(); ?>
