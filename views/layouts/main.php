666f2cf559cd9c017cea44fee7d9b4fbf6c9e37e
Каир Жумадилов <k.zhumadilov@g.nsu.ru>
2018-12-25 17:08:52 +0700

    menu layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,6 +1,7 @@
 <?php
 
 /* @var $this \yii\web\View */
+
 /* @var $content string */
 
 use app\widgets\Alert;
@@ -21,7 +22,7 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
@@ -41,17 +42,16 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-//            ['label' => Yii::t('app', 'Home'), 'url' => Yii::$app->homeUrl],
             ['label' => Yii::t('app', 'Category'), 'url' => ['/categories']],
-            Yii::$app->user->can(\app\models\User::ROLE_ADMINISTRATOR) ? (
-                '<li>'
-                . Html::a(Yii::t('app', 'Management'), ['/admin/index'], ['class' => 'btn btn-link'])
-                . '</li>'
-            ) : (
-            ''
-            ),
+            Yii::$app->user->can(\app\models\User::ROLE_ADMINISTRATOR) ? [
+                'label' => Yii::t('app', 'Management'),
+                'items' => [
+                    ['label' => 'Модели', 'url' => ['admin/index']],
+                    ['label' => 'Категории', 'url' => ['admin/list-category']],
+                ],
+            ] : (''),
             Yii::$app->user->isGuest ? (
-                    ''
+            ''
 //                ['label' => Yii::t('app', 'Sign in'), 'url' => ['/site/login']]
             ) : (
                 '<li>'
@@ -63,6 +63,12 @@ AppAsset::register($this);
                 . Html::endForm()
                 . '</li>'
             ),
+            ('<li style="padding: 15px 0">' .MultiLanguageWidget::widget([
+//                'widget_type' => 'classic', // classic or selector
+//                'image_type' => 'classic', // classic or rounded
+//                'width' => '18',
+//                'calling_controller' => $this->context
+            ]) . '</li>')
         ],
     ]);
     NavBar::end();
@@ -70,21 +76,9 @@ AppAsset::register($this);
 
     <div class="container">
 
-        <div class="clearfix">
-            <div class="pull-right">
-                <?= MultiLanguageWidget::widget([
-                    'widget_type' => 'classic', // classic or selector
-                    'image_type' => 'classic', // classic or rounded
-                    'width' => '18',
-                    'calling_controller' => $this->context
-                ]) ?>
-            </div>
-        </div>
-        <br>
-
-<!--        --><?//= Breadcrumbs::widget([
-//            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-//        ]) ?>
+        <!--        --><? //= Breadcrumbs::widget([
+        //            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        //        ]) ?>
         <?= Alert::widget() ?>
         <?= $content ?>
     </div>
