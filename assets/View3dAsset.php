251131c82f754dd08e66dd576fea4eb4a652aed1
Каир Жумадилов <k.zhumadilov@g.nsu.ru>
2019-02-07 17:59:08 +0700

    save state wireframe

--- assets/View3dAsset.php
+++ assets/View3dAsset.php
@@ -29,7 +29,7 @@ class View3dAsset extends AssetBundle
         'js/three/TrackballControls.js',
         'js/jquery.fancybox.min.js',
         'js/colorpicker.min.js',
-        'js/viewer.js?201902061508',
+        'js/viewer.js?201902071758',
         'js/label.js',
         'js/loader.object.js',
     ];
