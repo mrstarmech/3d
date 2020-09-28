73b082c16422fa32984ee5b4b28c50ba6626ed6f
vladislav-kazakov <vkazakov@phys.nsu.ru>
2020-09-28 16:08:26 +0700

    updated versions in Iframe3dAsset

--- assets/Iframe3dAsset.php
+++ assets/Iframe3dAsset.php
@@ -6,6 +6,7 @@ use yii\web\AssetBundle;
 
 /**
  * Class View3dAsset
+ *
  * @package app\assets
  */
 class Iframe3dAsset extends AssetBundle
@@ -13,15 +14,15 @@ class Iframe3dAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/loader.object.css?20200430',
+        'css/loader.object.css?2020043',
         'css/iframe.css',
         'css/jquery.fancybox.min.css',
         'css/colorpicker.css',
     ];
     public $js = [
         'js/three/dat.gui.min.js',
-        'js/three/three.min.js',
-        'js/three/OrbitControls.js',
+        'js/three/three.min.js?123',
+        'js/three/OrbitControls.js?123',
         'js/three/OBJLoader.js',
         'js/three/MTLLoader.js',
         'js/three/UTF8Loader.js',
@@ -35,9 +36,9 @@ class Iframe3dAsset extends AssetBundle
         'js/three/postprocessing/OutlinePass.js?123',
         'js/jquery.fancybox.min.js',
         'js/colorpicker.min.js',
-        'js/viewer.js?202005142',
+        'js/viewer.js?4',
         'js/label.js',
-        'js/loader.object.iframe.js?202005142',
+        'js/loader.object.iframe.js?4',
     ];
     public $depends = [
         'yii\web\YiiAsset',
